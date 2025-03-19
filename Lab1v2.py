1be339b1c5b3b521df1af6bbd963dc62f7463ec0
Jakub Mlaskawa <jakubxp9248@gmail.com>
2025-03-19 13:53:10 +0100

    function update

--- Lab1v2.py
+++ Lab1v2.py
@@ -7,20 +7,22 @@ def arr_game(a: np.ndarray):
     SL_D1 = min(max_in_rows)  # Najmniejsza wartość spośród największych wierszowych
     SL_D2 = max(min_in_cols)  # Największa wartość spośród najmniejszych kolumnowych
 
-    STR_D1 = np.argmin(max_in_rows)
-    STR_D2 = np.argmax(min_in_cols)
+    STR_D1 = (np.where(max_in_rows == SL_D1)[0] + 1).tolist()
+    STR_D2 = (np.where(min_in_cols == SL_D2)[0] + 1).tolist()
 
     if(SL_D1 == SL_D2):
         PS = True
     else:
         PS = False
 
-    return SL_D1, SL_D2, STR_D1 + 1, STR_D2 + 1, PS
+    return SL_D1, SL_D2, STR_D1, STR_D2, PS
 
-A = np.array([[4, 0, -1],
-              [0, -1, 3],
-              [1, 2, 1],
-              [3, 1, 0]])
+A = np.array([[4, 5, -2, 0, 3, 5],
+              [0, -3, 4, 8, 3, -6],
+              [2, 2, 0, 3, 0, 2],
+              [0, 6, -1, 2, 1, 4],
+              [3, 1, 3, 2, 0, -1],
+              [0, 3, 2, 2, 2, 0]])
 option_one = True
 option_two = False
 
