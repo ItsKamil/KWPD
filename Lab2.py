ba0aa13374df309b57a6c1febb1da06a30e06646
GitJackub <jakubxp9248@gmail.com>
2025-04-15 12:34:33 +0200

    zad 3, zad 4

--- Lab2.py
+++ Lab2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import linprog
 
 def mixed_strat2v2(a: np.ndarray, D2: bool):
 
@@ -54,3 +55,63 @@ arr = np.array([[4, -3],
 
 print("D2 clean strategies -> (saddle point, saddle value) =", mixed_strat2v2(arr, True))
 print("D1 clean strategies -> (saddle point, saddle value) =", mixed_strat2v2(arr, False))
+
+# zad 3
+
+def linear_prog(A, minimize):
+        if minimize: 
+            A_ub = A.T
+            b_ub = [1] * A.shape[1]
+            c = [-1] * A.shape[0]
+            bounds = [(0, None)] * A.shape[0]
+            nr = 1
+            symb = "y"
+            
+        else:
+            A_ub = -A
+            b_ub = [-1] * A.shape[0]
+            c = [1] * A.shape[1]
+            bounds = [(0, None)] * A.shape[1]
+            nr = 2
+            symb = "z"
+            
+        res = linprog(c, A_ub=A_ub, b_ub=b_ub, bounds=bounds, method='highs')
+
+        if res.success:
+            w = res.x
+            Sm = 1/sum(w)
+            strategy = w*Sm
+            result = f"Strategia mieszana gracza D{nr}: " + ", ".join(
+            [f"{symb}{i} = {p:.4f}" for i, p in enumerate(strategy, start=1)]
+            )
+            print(result)
+            print(f"Wartość gry (Sm): {Sm:.4f}")
+        else:
+            print("Nie udało się znaleźć rozwiązania.")
+
+print("---Zadanie 3---")
+
+A = np.array(
+    [[4, -3, 0],
+     [2, 5, 1]]
+     )
+
+linear_prog(A,True)
+linear_prog(A,False)
+
+
+# zad 4
+
+print("---Zadanie 4---")
+
+A = np.array([
+    [5, 3, 7, 6, 4, 2],
+    [8, 6, 1, 3, 5, 7],
+    [7, 2, 9, 4, 5, 6],
+    [10, 5, 3, 9, 6, 7],
+    [9, 8, 4, 6, 11, 7],
+    [11, 9, 5, 8, 7, 6]
+])
+
+linear_prog(A,True)
+linear_prog(A,False)
